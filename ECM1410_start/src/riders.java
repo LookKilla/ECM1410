94d2a291d1c46eeef25c83a380ae73cc868d8482
Samuel <160143980+LookKilla@users.noreply.github.com>
2024-02-21 12:36:18 +0000

    Update riders.java

--- ECM1410_start/src/riders.java
+++ ECM1410_start/src/riders.java
@@ -4,6 +4,13 @@ public class rider {
   private int yearOfBirth;
   private int riderID;
 
-  
+  public int createRider(int teamID, String name, int yearOfBirth){
+    this.teamID = teamID;
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+
+    // TODO Make riderID
+    return riderID;
+  }
   
 }
