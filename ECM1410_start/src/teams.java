672239c7cfd70a6d42aea2e90c7f7b2cc3aa0a1b
Samuel <160143980+LookKilla@users.noreply.github.com>
2024-02-21 12:30:02 +0000

    Update teams.java

--- ECM1410_start/src/teams.java
+++ ECM1410_start/src/teams.java
@@ -1,9 +1,14 @@
 public class teams{
   private int teamID;
-  private string name;
-  private string description;
+  private String name;
+  private String description;
   private int[] riders;
 
+  public int createTeam(String name, String description){
+    this.name = name;
+    this.description = description;
 
+    return teamID;
+  }
   
 }
