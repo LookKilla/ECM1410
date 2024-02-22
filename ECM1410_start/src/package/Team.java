e6a36abbad929121c772b0be45875f1a9a5bc4d1
Samuel <160143980+LookKilla@users.noreply.github.com>
2024-02-22 09:42:43 +0000

    Update Team.java

--- ECM1410_start/src/package/Team.java
+++ ECM1410_start/src/package/Team.java
@@ -1,23 +1,3 @@
-public class teams{
-  private int teamID;
-  private String name;
-  private String description;
-  private int[] riders;
-
-  public int createTeam(String name, String description){
-    this.name = name;
-    this.description = description;
-
-    return teamID;
-  }
-
-  public int[] getTeamRiders(int teamID){
-    return riders;
-  }
-
-  
-  
-}
 package src;
 
 import java.util.HashMap;
