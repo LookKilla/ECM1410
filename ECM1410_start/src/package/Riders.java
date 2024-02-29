7efd1f6d7493f981c81f4192cb9402c1b4ee13bc
barni701 <146190511+barni701@users.noreply.github.com>
2024-02-29 18:07:45 +0000

    Update Riders.java

--- ECM1410_start/src/package/Riders.java
+++ ECM1410_start/src/package/Riders.java
@@ -2,7 +2,7 @@ package cycling;
 
 public class Riders {
 
-    private static int lastUsedRiderID; // Variable to keep track of the last used team ID
+    private int lastUsedRiderID =1; // Variable to keep track of the last used team ID
 
     private String Name;
     private int yearOfBirth;
@@ -10,18 +10,24 @@ public class Riders {
     private int RiderId;
 
 
-    public int getRiderId(){
-        return RiderId;
-    }
+    //getters
+    public int getYearOfBirth(){return yearOfBirth;}
+    public String getName(){return Name;}
+    public int getRiderId(){return RiderId;}
+
+    //setters
+    public void setName(String Name){this.Name = Name;}
+    public void setRiderId(int RiderId){this.RiderId =RiderId;}
+    public void setYearOfBirth(int yearOfBirth){this.yearOfBirth = yearOfBirth;}
+
 
     public Riders(){
 
         CyclingPortalImpl Portal = new CyclingPortalImpl();
-        lastUsedRiderID = Portal.lastUsedTeamID;
 
     }
 
-    public int createRider(String name, int yearOfBirth) throws IllegalNameException, InvalidNameException {
+    public int createTeam(String name, int yearOfBirth) throws IllegalNameException, InvalidNameException {
         int riderID = generateRiderID();
 
 
@@ -33,7 +39,7 @@ public class Riders {
         return riderID;
     }
 
-    private static int generateRiderID() {
+    private int generateRiderID() {
         // Increment the last used team ID to generate a new unique ID
         return lastUsedRiderID + 1;
     }
