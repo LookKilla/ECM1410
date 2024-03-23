7b717eb46dee73b023c5faa364f6634b4cd6ff43
barni701 <146190511+barni701@users.noreply.github.com>
2024-03-23 20:03:50 +0000

    Update Team.java

--- ECM1410_start/src/package/Team.java
+++ ECM1410_start/src/package/Team.java
@@ -1,16 +1,12 @@
 package cycling;
 
-import java.util.Arrays;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Team {
+public class Team implements Serializable {
 
-    private Team[] Teams;
-
-    private static int lastUsedTeamID; // Variable to keep track of the last used team ID
-    
     private String Name;
     private String Description;
-
     private int TeamId;
     private ArrayList<Integer> RiderIDs;
 
@@ -22,10 +18,12 @@ public class Team {
     public int getTeamId(){return TeamId;}
     public ArrayList<Integer> getRiderIDs(){return RiderIDs;}
 
+
     //setters
     public void setName(String Name){this.Name = Name;}
     public void setDescription(String Description){this.Description = Description;}
     public void setTeamId(int TeamId){this.TeamId = TeamId;}
+
     public void addRider(int riderID){this.RiderIDs.add(riderID);}
 
 
@@ -40,15 +38,15 @@ public class Team {
         return arrRiders;
     }
 
-    public int createTeam(String name, String description) throws IllegalNameException, InvalidNameException {
-        int teamID = generateTeamID();
+    public int createTeam(String name, String description){
 
-        RiderIDs = new int[20];
+
+        RiderIDs = new ArrayList<>();
         this.Name = name;
+        int teamID = generateID();
         this.Description = description;
         this.TeamId = teamID;
 
-        ++lastUsedTeamID;
         return teamID;
     }
 
@@ -62,21 +60,10 @@ public class Team {
         return id;
     }
 
-    private int getLastUsedTeamID() {
-        // Find the maximum value in the teamIDs array
-        // Note: This assumes that the teamIDs array is not sparse (i.e., no gaps in the IDs)
-        int maxID = Arrays.stream(teamIDs).max().orElse(0);
 
-        // Update the lastUsedTeamID
-        lastUsedTeamID = maxID;
-
-        return lastUsedTeamID;
-    }
 
     @Override
     public String toString() {
         return "Team "+Name+": "+ Description+ ": "+TeamId;
     }
-
-
 }
